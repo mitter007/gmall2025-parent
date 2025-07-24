4e512bab920d78a9d122088e40407a6959e462c1
JiaoWT <jiaowt762@163.com>
2025-07-24 11:51:07 +0800

    流量域搜索关键词粒度页面浏览各窗口汇总表

--- realtime-gmall/src/main/java/com/atguigu/gmall/realtime/common/constant/Constant.java
+++ realtime-gmall/src/main/java/com/atguigu/gmall/realtime/common/constant/Constant.java
@@ -40,11 +40,12 @@ public class Constant {
     public static final String TOPIC_DWD_TRADE_ORDER_REFUND = "dwd_trade_order_refund";
 
     public static final String TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS = "dwd_trade_refund_payment_success";
+    public static final String DWS_TRAFFIC_SOURCE_KEYWORD_PAGE_VIEW_WINDOW = "dws_traffic_source_keyword_page_view_window";
 
     public static final String TOPIC_DWD_USER_REGISTER = "dwd_user_register";
 
     public static final String DORIS_FE_NODES = "hadoop202:7030";
 
-    public static final String DORIS_DATABASE = "gmall_realtime";
+    public static final String DORIS_DATABASE = "gmall2025_realtime";
 
 }
---
+++ realtime-gmall/src/main/java/com/atguigu/gmall/realtime/dws/DwsTrafficSourceKeywordPageViewWindow.java
@@ -0,0 +1,100 @@
+package com.atguigu.gmall.realtime.dws;
+
+import com.atguigu.gmall.realtime.common.base.BaseSQL;
+import com.atguigu.gmall.realtime.common.constant.Constant;
+import com.atguigu.gmall.realtime.common.util.FlinkSQLUtil;
+import com.atguigu.gmall.realtime.dws.function.KeywordUDTF;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * ClassName: DwsTrafficSourceKeywordPageViewWindow
+ * Package: com.atguigu.gmall.realtime.dws
+ * Description:
+ *
+ * @Author JWT
+ * @Create 2025/7/24 11:10
+ * @Version 1.0
+ */
+public class DwsTrafficSourceKeywordPageViewWindow extends BaseSQL {
+    public static void main(String[] args) {
+        new DwsTrafficSourceKeywordPageViewWindow()
+                .start(1005,
+                        4,
+                        Constant.DWS_TRAFFIC_SOURCE_KEYWORD_PAGE_VIEW_WINDOW);
+    }
+
+    @Override
+    public void handle(StreamTableEnvironment tableEnv) {
+        //TODO 注册自定义函数到表执行环境中
+        tableEnv.createTemporarySystemFunction("ik_analyze", KeywordUDTF.class);
+        readPageLog(tableEnv);
+        searchTable(tableEnv);
+        splitKeyword(tableEnv);
+        //TODO 分组、开窗、聚合
+        Table resTable = tableEnv.sqlQuery("SELECT \n" +
+                "     date_format(window_start, 'yyyy-MM-dd HH:mm:ss') stt,\n" +
+                "     date_format(window_end, 'yyyy-MM-dd HH:mm:ss') edt,\n" +
+                "     date_format(window_start, 'yyyy-MM-dd') cur_date,\n" +
+                "     keyword,\n" +
+                "     count(*) keyword_count\n" +
+                "  FROM TABLE(\n" +
+                "    TUMBLE(TABLE split_table, DESCRIPTOR(et), INTERVAL '10' second))\n" +
+                "  GROUP BY window_start, window_end,keyword");
+//        resTable.execute().print();
+        //TODO 将聚合的结果写到Doris中
+        tableEnv.executeSql("create table dws_traffic_source_keyword_page_view_window(" +
+                "  stt string, " +  // 2023-07-11 14:14:14
+                "  edt string, " +
+                "  cur_date string, " +
+                "  keyword string, " +
+                "  keyword_count bigint " +
+                ")with(" +
+                " 'connector' = 'doris'," +
+                " 'fenodes' = '" + Constant.DORIS_FE_NODES + "'," +
+                "  'table.identifier' = '" + Constant.DORIS_DATABASE + ".dws_traffic_source_keyword_page_view_window'," +
+                "  'username' = 'root'," +
+                "  'password' = 'aaaaaa', " +
+                "  'sink.properties.format' = 'json', " +
+                "  'sink.buffer-count' = '4', " +
+                "  'sink.buffer-size' = '4086'," +
+                "  'sink.enable-2pc' = 'false', " + // 测试阶段可以关闭两阶段提交,方便测试
+                "  'sink.properties.read_json_by_line' = 'true' " +
+                ")");
+        resTable.executeInsert("dws_traffic_source_keyword_page_view_window");
+
+    }
+
+    private static void readPageLog(StreamTableEnvironment tableEnv) {
+        //TODO 从页面日志事实表中读取数据 创建动态表  并指定Watermark的生成策略以及提取事件时间字段
+
+//        {"common":{"ar":"14","os":"Android 13.0","ch":"xiaomi","is_new":"0","md":"realme Neo2","mid":"mid_210","vc":"v2.1.134","ba":"realme","sid":"a34605b4-6bb7-4564-ad1f-16e3692ee624"},"start":{"entry":"icon","open_ad_skip_ms":40622,"open_ad_ms":7764,"loading_time":12883,"open_ad_id":18},"ts":1749517338779}
+        tableEnv.executeSql("create table page_log(\n" +
+                "     common map<string,string>,\n" +
+                "     page map<string,string>,\n" +
+                "     ts bigint,\n" +
+                "     et as TO_TIMESTAMP_LTZ(ts, 3),\n" +
+                "     WATERMARK FOR et AS et\n" +
+                ")" + FlinkSQLUtil.getKafkaDDL(Constant.TOPIC_DWD_TRAFFIC_PAGE, "dws_traffic_source_keyword_page_view_window"));
+    }
+
+    private static void searchTable(StreamTableEnvironment tableEnv) {
+        //TODO 过滤出搜索行为
+        Table searchTable = tableEnv.sqlQuery("select \n" +
+                "   page['item']  fullword,\n" +
+                "   et\n" +
+                "from page_log\n" +
+                "where page['last_page_id'] = 'search' and page['item_type'] ='keyword' and page['item'] is not null");
+        tableEnv.createTemporaryView("search_table", searchTable);
+
+    }
+
+    private static void splitKeyword(StreamTableEnvironment tableEnv) {
+        System.out.println("********2**************");
+        Table splitTable = tableEnv.sqlQuery("SELECT keyword,et FROM search_table,\n" +
+                "LATERAL TABLE(ik_analyze(fullword)) t(keyword)");
+        tableEnv.createTemporaryView("split_table", splitTable);
+    }
+
+
+}
